65740be070b7082dcc48b7dcd9d6e05232eb7b15
Eleocraft <eliseo.cailloux@gmx.de>
2024-10-13 19:38:35 +0200

    test 4

--- OpenHD/ohd_video/inc/gst_helper.hpp
+++ OpenHD/ohd_video/inc/gst_helper.hpp
@@ -710,11 +710,11 @@ static std::string createImageSavingCodec(
     const int seconds, const std::string& out_filename) {
   std::stringstream ss;
   // don't forget the white space before the " t." !
-  ss << " t. ! queue ! fakesink sync=false";
-  /*
   ss << " t. ! queue ! decodebin ! videorate ! videoscale ! ";
   ss << "video/x-raw,width=960,height=540,framerate=1/";
   ss << seconds;
+  ss << " ! fakesink sync=false";
+  /*
   ss << " ! jpegenc ! multifilesink location=";
   ss << out_filename << "-%02d.jpg";*/
   return ss.str();
